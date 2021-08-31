4296b4849176ca817e32c526ce006df3e1031d4a
AHTOH2001 <antonlapytko@gmail.com>
2021-08-31 15:13:49 +0300

    Removed redundant comments

--- iTechArt_project_backend/urls.py
+++ iTechArt_project_backend/urls.py
@@ -18,8 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include('FrontieBontie.urls')),
     path('', include('cookieapp.urls')),
 ]
